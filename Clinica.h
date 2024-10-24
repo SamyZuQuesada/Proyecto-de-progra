c23bfc4fb2cc8731012c2ae032cc62d872bad878
Samira Zuñiga Quesada <samira.zuniga.quesada@est.una.ac.cr>
2024-10-24 13:47:05 -0600

    Update Clinica.h

--- Clinica.h
+++ Clinica.h
@@ -14,7 +14,7 @@ private:
 	string direccionClinica;
 	string especialidades;
 	string correoClinica;
-	string telefonoClinica;
+	int telefonoClinica;
 
 public:
 	Clinica()
@@ -23,13 +23,14 @@ public:
 		this->direccionClinica = "Nicoya, Guanacaste";
 		this->especialidades = "Medicina General, Odontologia, Nutricion, Pediadria, Psicologia";
 		this->correoClinica = "santaquantumcode@hotmail.com";
-		this->telefonoClinica = 2685-5314;
+		this->telefonoClinica = 26855314;
 	}
 
 	void menuClinica()
 	{
 
 		int opcionClinica;
+		bool salir = false;
 		ContenedorServicios contenedorServicios;
 		ContenedorMedicos contenedorMedicos;
 		ContenedorPacientes contenedorPacientes;
@@ -44,28 +45,28 @@ public:
 			cout << "================== DANIEL ELIZONDO GUTIERREZ ===================" << endl;
 			cout << "================= SAMIRA ZUNIGA QUESADA ========================" << endl;
 			cout << "================== NAEL BARRANTES DIAZ =========================" << endl;
-			cout << "================== LIZZY NOGUERA MEDINA ========================" << endl;
+			cout << "================== LISSY NOGUERA MEDINA ========================" << endl;
 			cout << "================================================================" << endl;
 
 			// Pausar para que el usuario presione Enter
 			cout << "Presione enter para poner continuar..." << endl;
 			cin.get(); // Espera que el usuario presione Enter
 			system("cls");
-			
 
 
 			system("cls");
-			cout << "================================================================" << endl;
-			cout << "=================BIENVENIDOS/BIENVENIDAS========================" << endl;
-			cout << "=================CLINICA SAN QUANTUM CODE=======================" << endl;
-			cout << "======================MENU PRINCIPAL============================" << endl;
-			cout << "================================================================" << endl;
+			cout << "----------------------------------------------------------------" << endl;
+			cout << "------------------------BIENVENID@s-----------------------------" << endl;
+			cout << "------------------CLINICA SAN QUANTUM CODE----------------------" << endl;
+			cout << "-----------------------MENU PRINCIPAL---------------------------" << endl;
+			cout << "----------------------------------------------------------------" << endl;
 			cout << "1-Gestion de servicios: " << endl;
 			cout << "2-Gestion de medicos: " << endl;
 			cout << "3-Gestion de pacientes: " << endl;
 			cout << "4-Gestion de citas: " << endl;
 			cout << "5-Salir" << endl;
-			cout << "================================================================" << endl;
+			cout << "-----------------------------------------------------------------" << endl;
+			cout << "-----------------------------------------------------------------" << endl;
 			cout << "Ingrese su opcion: ";
 			cin >> opcionClinica;
 
@@ -95,62 +96,55 @@ public:
 
 			case 4:
 			{
+				//contenedorCitas.MenuCitas();
 				system("pause");
 				break;
 			}
 
 			case 5:
 			{
-				bool salir = false;
-				int opcion;
-				while (!salir)
-				{
-					cout << "Desea salir de nuestro menu?" << endl;
-					cout << "1-SI" << endl;
-					cout << "0-NO" << endl;
-					cin >> opcion;
+				int opcionSalir;
+				cout << "..........................................." << endl;
+				cout << "Desea salir de nuestro menu?" << endl;
+				cout << "1-SI" << endl;
+				cout << "0-NO" << endl;
+				cout << "..........................................." << endl;
+				cin >> opcionSalir;
 
-					switch (opcion)
-					{
-					case 1:
-					{
-						salir = true;
-						cout << "¡Gracias por usar nuestra clinica!" << endl;
-						cout << "Estamos ubicados en: Nicoya, Guanacaste " << endl;
-						cout << "Nuestro correo es: sanaquantumcode@hotmail.com" << endl;
-						cout << "Recordar que brindamos nuestros servicios de: Medicina General, Odontologia, Nutricion, Pediadria, Psicologia" << endl;
-						cout << "Nuestro numero telefonico es: 2685-5314" << endl;
-						cout << "¡Clinica San Quantum Code le agradace por confiar en nosotros! " << endl;
-						cout << "!LO ESPERAMOS PRONTO!" << endl;
-						system("pause");
-						break;
-					}
-					case 0:
-					{
-						salir = false;
-						cout << "¡GRACIAS POR CONTINUAR EN NUESTRO MENU!" << endl;
-						system("pause");
-						break;
-					}
-					default:
-					{
-						cout << "OPCION INVALIDA" << endl;
-						system("pause");
-						break;
-					}
-					}
+				if (opcionSalir == 1)
+				{
+					salir = true;
+					cout << "¡Gracias por usar nuestra clinica!" << endl;
+					cout << "Estamos ubicados en: Nicoya, Guanacaste " << endl;
+					cout << "Nuestro correo es: sanquantumcode@hotmail.com" << endl;
+					cout << "Recordar que brindamos nuestros servicios de: Medicina General, Odontologia, Nutricion, Pediadria, Psicologia" << endl;
+					cout << "Nuestro numero telefonico es: 26855314" << endl;
+					cout << "¡Clinica San Quantum Code le agradace por confiar en nosotros! " << endl;
+					cout << "!LO ESPERAMOS PRONTO!" << endl;
+					system("pause");
 				}
-			}
-			default:
-			{
-				cout << "==¡OPCION INCORRECTA!==" << endl;
-				cout << "VUELVA A INTENTAR CON OTRA OPCION..." << endl;
+				else if (opcionSalir == 0) {
+
+					cout << "¡GRACIAS POR CONTINUAR EN NUESTRO MENU!" << endl;
+					system("pause");
+				}
+				else
+				{
+					cout << "¡Opcion no valida!" << endl;
+					system("pause");
+				}
 				break;
 			}
 
+			default:
+			{
+				cout << "--¡OPCION INCORRECTA!--" << endl;
+				cout << "VUELVA A INTENTAR CON OTRA OPCION..." << endl;
 			}
 
-		} while (opcionClinica != 5);
+			}
+
+		} while (!salir);
 
 
 	}
