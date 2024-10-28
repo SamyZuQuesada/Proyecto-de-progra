171cb3b33e1749497190986090c23425373984a8
Lyss1A <lissynoguera2212@gmail.com>
2024-10-28 15:56:21 -0600

    Update Pacientes.h

--- Pacientes.h
+++ Pacientes.h
@@ -5,43 +5,55 @@
 class Pacientes : public Persona
 {
 private:
-
+	int cedula;
 	string apellido;
 	string fechaRegistro;
 
 public:
-	Pacientes(string _nombre, string _telefono, string _correo, int _cedula, string _apellido, string _fechaRegistro) : Persona(_nombre, _telefono, _correo, _cedula)
+	Pacientes(string _nombre, string _telefono, string _correo, string _apellido, int _cedula, string _fechaRegistro) : Persona(_nombre, _correo, _telefono)
 	{
-
+		this->cedula = _cedula;
 		this->apellido = _apellido;
 		this->fechaRegistro = _fechaRegistro;
 	}
 
 	//metodos setter y getter
 
-	void setApellido(string _apellido)
+	void setCedula(const int _cedula)
+	{
+		this->cedula = _cedula;
+	}
+
+	const int getCedula() const
+	{
+		return this->cedula;
+	}
+
+	void setApellido(const string _apellido)
 	{
 		this->apellido = _apellido;
 	}
-	string getApellido() const
+	const string getApellido() const
 	{
 		return this->apellido;
 	}
 
-	void setFechaRegistro(string _fechaRegistro)
+	void setFechaRegistro(const string _fechaRegistro)
 	{
 		this->fechaRegistro = _fechaRegistro;
 	}
-	string getFechaRegistro()const
+	const string getFechaRegistro() const
 	{
 		return this->fechaRegistro;
 	}
 
+	//Metodo que muestra datos del paciente utilizando polimosrfismo
 	void getMostrarDatos() const override
 	{
 		cout << "................Datos del paciente............" << endl;
 		Persona::getMostrarDatos();
 		cout << "Apellido: " << getApellido() << endl;
+		cout << "Cedula: " << getCedula() << endl;
 		cout << "Fecha de registro: " << getFechaRegistro() << endl;
 		cout << "..............................................." << endl;
 	}
