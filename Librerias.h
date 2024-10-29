e287dc735440f023fa79675db689dd7b392db48b
Gvrh05 <delizondogutierrez@gmail.com>
2024-10-29 17:43:34 -0600

    Update Librerias.h

--- Librerias.h
+++ Librerias.h
@@ -4,4 +4,10 @@
 #include <string>
 #include <fstream> 
 #include<sstream>
+#include "Citas.h"
+#include "Persona.h"
+#include "ContenedorMedicos.h"
+#include "ContenedorServicios.h"
+#include "ContenedorPacientes.h"
+
 using namespace std;
