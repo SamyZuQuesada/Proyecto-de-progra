5ca209a149d34d8358d5b4e7ea9f8c74846f976c
Samira Zuñiga Quesada <samira.zuniga.quesada@est.una.ac.cr>
2024-10-29 20:13:52 -0600

    Update Librerias.h

--- Librerias.h
+++ Librerias.h
@@ -4,10 +4,4 @@
 #include <string>
 #include <fstream> 
 #include<sstream>
-#include "Citas.h"
-#include "Persona.h"
-#include "ContenedorMedicos.h"
-#include "ContenedorServicios.h"
-#include "ContenedorPacientes.h"
-
 using namespace std;
